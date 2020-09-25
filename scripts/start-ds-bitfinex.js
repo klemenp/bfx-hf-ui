60cdb2b83fbd1b451f1d31ff12d942b275999b5b
klemenp <klemen@your.md>
2020-09-25 12:00:16 +0200

    locally run with pg and additional npm run start

--- scripts/start-ds-bitfinex.js
+++ scripts/start-ds-bitfinex.js
@@ -6,7 +6,7 @@ require('bfx-hf-util/lib/catch_uncaught_errors')
 const HFDB = require('bfx-hf-models')
 const os = require('os')
 const DataServer = require('bfx-hf-data-server')
-const HFDBLowDBAdapter = require('bfx-hf-models-adapter-lowdb')
+const HFDBSQLAdapter = require('bfx-hf-models-adapter-sql')
 const { schema: HFDBBitfinexSchema } = require('bfx-hf-ext-plugin-bitfinex')
 
 const dir = `${os.homedir()}/.honeyframework`;
